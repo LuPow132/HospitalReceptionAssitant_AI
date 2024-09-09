1566e6eb607ddde2f487e1ac2982e1b870f37272
LuPow132 <68886157+LuPow132@users.noreply.github.com>
2024-09-09 22:00:31 +0700

    Create Function_calling.py

---
+++ Example/OpenAI/Function_calling/Function_calling.py
@@ -0,0 +1,56 @@
+from openai import OpenAI
+import json
+import Key
+
+client = OpenAI(api_key=Key.OPEN_AI_KEY)
+
+conversation = [
+    {"role": "system", "content": "You are a helpful assistant."},
+]
+
+function_descriptions = [
+    {
+        "name": "get_user_info",
+        "description": "Get User basic information for making an appointment",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "Height": {
+                    "type": "string",
+                    "description": "Height in cm",
+                },
+                "Weight": {
+                    "type": "string",
+                    "description": "Weight in kg",
+                },
+                "Symptoms": {
+                    "type": "string",
+                    "description": "Symptoms of what user feel",
+                },
+            },
+            "required": ["Height", "Weight","Symptoms"],
+        },
+    }
+]
+
+while True:
+    # Get user input
+    user_input = input("User: ")
+    
+    # Append user message to conversation
+    conversation.append({"role": "user", "content": user_input})
+    
+    # Get the AI's response
+    completion = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=conversation,
+        functions=function_descriptions,
+        function_call="auto",  # specify the function call
+    )
+    
+    # Append AI's response to conversation
+    ai_response = completion.choices[0].message.content
+    conversation.append({"role": "assistant", "content": ai_response})
+    
+    # Print the AI's response
+    print(f'AI: {completion.choices[0].message}')
