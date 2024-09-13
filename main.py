3a26a5311e54508372e4b536bd6365d36561c05a
LuPow132 <68886157+LuPow132@users.noreply.github.com>
2024-09-13 22:56:48 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -2,14 +2,21 @@ from openai import OpenAI
 import json
 import Key
 import csv
+import asyncio
+import edge_tts
+from playsound import playsound
+import os
 
-csvDB = "CSV_Test/sample_data.csv"
+csvDB = "Output/appointment.csv"
 rows = []
+
+VOICE = "th-TH-PremwadeeNeural"
+OUTPUT_FILE = "Output/output.mp3"
 
 client = OpenAI(api_key=Key.OPEN_AI_KEY)
 
 conversation = [
-    {"role": "system", "content": "You are a hospital receptionist anime girl. Your job is to assist patient in Thai. You work at BUU hospital"},
+    {"role": "system", "content": "You are a friendly hospital receptionist anime girl who want to make everyone happy. Your job is to assist patient in Thai. You work at โรงพยาบาลบูรพา hospital"},
 ]
 
 function_descriptions = [
@@ -37,6 +44,14 @@ function_descriptions = [
     }
 ]
 
+async def generate_audio(TEXT):
+    print("Generate TTS")
+    communicate = edge_tts.Communicate(TEXT, VOICE,pitch='+35Hz')
+    await communicate.save(OUTPUT_FILE)
+    playsound(OUTPUT_FILE)
+    os.remove(OUTPUT_FILE)
+    print("Finish Generate TTS")
+
 def make_an_appointment(height,weight,Symtomps):
     print(f'parameter{height,weight,Symtomps}')
     ID_Card_Info = read_ID_card().split(",")
@@ -54,7 +69,7 @@ def make_an_appointment(height,weight,Symtomps):
         print("Queue Full")
 
 def read_ID_card():
-    with open('data/Example_data/Exampe_ID_card_info.txt', 'r', encoding="utf8") as file:
+    with open('Input/Exampe_ID_card_info.txt', 'r', encoding="utf8") as file:
         content = file.read()
         return content
     
@@ -81,6 +96,10 @@ def write_csv(content):
         csvwriter = csv.writer(csvfile)
         # writing the data as a new row (content is wrapped in another list)
         csvwriter.writerow(content)
+def reset_conversation():
+    conversation = [
+    {"role": "system", "content": "You are a friendly hospital receptionist anime girl who want to make everyone happy. Your job is to assist patient in Thai. You work at BUU hospital"},
+]
     
 while True:
     # Get user input
@@ -104,7 +123,6 @@ while True:
     # Print the AI's response
     print(f'AI: {ai_response.choices[0].message.content}')
     # print(f'Function{ai_response.choices[0].message.function_call}')
-
     if(ai_response.choices[0].message.function_call != None):
         if(ai_response.choices[0].message.function_call.name == "make_an_appointment"):
             parameter = json.loads(ai_response.choices[0].message.function_call.arguments)
@@ -112,3 +130,6 @@ while True:
             weight = float(parameter["weight"])
             symptoms = parameter["symptoms"]
             make_an_appointment(height,weight,symptoms)
+            reset_conversation()
+    else:
+        asyncio.run(generate_audio(ai_response.choices[0].message.content))
